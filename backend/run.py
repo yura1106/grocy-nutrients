5ca0622bec129ad24f53b0d15fe26339df8a79bb
Yurii Kuznietsov <yurii.kuznietsov11@gmail.com>
2026-02-11 19:53:07 +0200

    fix bd initialization

--- backend/run.py
+++ backend/run.py
@@ -1,9 +1,4 @@
 import uvicorn
-from app.db.init_db import init_db
 
 if __name__ == "__main__":
-    # Initialize the database
-    init_db()
-    
-    # Run the application
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True) 
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
